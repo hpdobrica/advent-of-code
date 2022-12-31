684366a2d30542b10c6a2a9509a0a78e0641e02f
Dobrica Savic <dobrica.savic@sysdig.com>
2022-12-31 12:21:49 +0100

    task 6 part 2

--- 06/main.go
+++ 06/main.go
@@ -13,33 +13,49 @@ func main() {
 	check(err)
 	defer inputFile.Close()
 
-	index := findSignalStartIndex(inputFile)
+	packetIndex, messageIndex := findSignalStartIndex(inputFile)
 
-	fmt.Println("Sequence start index is", index+1)
+	fmt.Println("Packet sequence start index is", packetIndex+1)
+	fmt.Println("Message sequence start index is", messageIndex+1)
 
 }
 
-func findSignalStartIndex(inputFile *os.File) int {
-	sequenceIndex := 0
-	fourLetterSequence := ""
+var PACKET_SEQUENCE_LENGTH = 4
+var MESSAGE_SEQUENCE_LENGTH = 14
+
+func findSignalStartIndex(inputFile *os.File) (int, int) {
+	packetSequenceIndex := 0
+	messageSequenceIndex := 0
+	packetSequence := ""
+	messageSequence := ""
 	forCharOfFile(inputFile, func(i int, c string) error {
-		if len(fourLetterSequence) == 4 {
-			fourLetterSequence = fourLetterSequence[1:]
+		if len(packetSequence) == PACKET_SEQUENCE_LENGTH {
+			packetSequence = packetSequence[1:]
 		}
-		fourLetterSequence += c
-		if isStartSequence(fourLetterSequence) {
-			sequenceIndex = i
+		if len(messageSequence) == MESSAGE_SEQUENCE_LENGTH {
+			messageSequence = messageSequence[1:]
+		}
+
+		packetSequence += c
+		messageSequence += c
+
+		if packetSequenceIndex == 0 && checkSequence(packetSequence, PACKET_SEQUENCE_LENGTH) {
+			packetSequenceIndex = i
+		}
+
+		if messageSequenceIndex == 0 && checkSequence(messageSequence, MESSAGE_SEQUENCE_LENGTH) {
+			messageSequenceIndex = i
 			return io.EOF
 		}
 
 		return nil
 
 	})
-	return sequenceIndex
+	return packetSequenceIndex, messageSequenceIndex
 }
 
-func isStartSequence(s string) bool {
-	if len(s) < 4 {
+func checkSequence(s string, length int) bool {
+	if len(s) < length {
 		return false
 	}
 	uniques := make(map[string]bool)
